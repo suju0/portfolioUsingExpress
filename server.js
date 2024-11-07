33097fcb36ad05e0e53be48a7a9856441d7c575a
suju0 <88792927+suju0@users.noreply.github.com>
2024-11-07 16:15:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+var express= require('express');
+var app = express();
+app.set('view engine','ejs');
+app.set('views','./views');
+app.get('/',function(req,res){
+    res.render('index')
+});
+app.listen(8080);
